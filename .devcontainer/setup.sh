f0f6d0b2448b2f2fd446c38fe5c52fcb1c95375b
zoolaph <94008067+zoolaph@users.noreply.github.com>
2025-07-17 14:27:19 +0000

    ruby pqckqge

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,9 +5,16 @@ echo "Installing AWS CLI..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+rm -rf aws awscliv2.zip
+
+# Update package list and enable universe repo
 sudo apt-get update
-sudo apt-get install ruby-full
-# Confirm AWS CLI installation
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository universe
+sudo apt-get update
+
+# Install Ruby
+sudo apt-get install -y ruby-full
 aws --version
 
 # Configure AWS credentials (Optional: Set up default profile)
